24e9d294836bf4310e0227e43de453385ca8bd54
Grace Shih <vocalise@gmail.com>
2018-01-09 18:56:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (isLowerCase(string))
+  if (string === uppercase)
     return "I can't hear you!"
-  else if (isUpperCase(string))
+  elseif (string === lowercase)
     return "YES INDEED!"
-    else if (string.equalsTo("I love you, too"))
+    else if (string === "I love you, Grandma.")
+    return "I love you, too."
 }
